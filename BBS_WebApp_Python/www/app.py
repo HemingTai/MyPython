f8a3eec02c69a671e16ff13ec9e7feac50e1ab14
heming_tai <heming_tai@163.com>
2017-11-24 18:00:59 +0800

    update project status

---
+++ BBS_WebApp_Python/www/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Hem1ng'
+
+import logging; logging.basicConfig(level=logging.INFO)
+import asyncio, os, json, time
+from datetime import datetime
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET','/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9002)
+    logging.info('server started at http://127.0.0.1:9002...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
--- Web.py
+++ Web.py
@@ -31,7 +31,7 @@
 
 
 # application()函数就是符合WSGI标准的一个HTTP处理函数，它接收两个参数：
-# environ：一个包含所有HTTP请求信息的dict对象；
+# enviroment：一个包含所有HTTP请求信息的dict对象；
 # start_response：一个发送HTTP响应的函数。
 def application(enviroment, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
